bffa1cda9150ca65900a31186e14cfcf7d5bedd9
DylJoanna022 <joanna.dyll@gmail.com>
2021-07-29 11:19:30 +0200

    HempFC

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,126 @@
 const WEIGHT_UNIT = "gr"
 const LIQUID_UNIT = "ml"
 
+const HEMP_FLOWER = [
+    {
+    name: "Brooklyn 66 CBD",
+    grammarges: [
+        { grammarge: 1.5, price: 4.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 8.00, unit: WEIGHT_UNIT },
+        { grammarge: 10, price: 22.00, unit: WEIGHT_UNIT },
+        { grammarge: 25, price: 40.00, unit: WEIGHT_UNIT },
+    ],
+    name: "New Orleans Skunk",
+    grammarges: [
+        { grammarge: 1.5, price: 4.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 8.00, unit: WEIGHT_UNIT },
+        { grammarge: 10, price: 22.00, unit: WEIGHT_UNIT },
+        { grammarge: 25, price: 40.00, unit: WEIGHT_UNIT },
+    ],
+    name: "Philadelphia Relax CBD",
+    grammarges: [
+        { grammarge: 1.5, price: 4.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 8.00, unit: WEIGHT_UNIT },
+        { grammarge: 10, price: 22.00, unit: WEIGHT_UNIT },
+        { grammarge: 25, price: 40.00, unit: WEIGHT_UNIT },
+    ],
+    name: "New York Ice CBD",
+    grammarges: [
+        { grammarge: 1.5, price: 4.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 8.00, unit: WEIGHT_UNIT },
+        { grammarge: 10, price: 22.00, unit: WEIGHT_UNIT },
+        { grammarge: 25, price: 40.00, unit: WEIGHT_UNIT },
+    ],
+    name: "Orange Bud CBD",
+    grammarges: [
+        { grammarge: 1.5, price: 4.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 8.00, unit: WEIGHT_UNIT },
+        { grammarge: 10, price: 22.00, unit: WEIGHT_UNIT },
+        { grammarge: 25, price: 40.00, unit: WEIGHT_UNIT },
+    ],
+    name: "Ijoint Kush CBD",
+    grammarges: [
+        { grammarge: 1.5, price: 4.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 8.00, unit: WEIGHT_UNIT },
+        { grammarge: 10, price: 22.00, unit: WEIGHT_UNIT },
+        { grammarge: 25, price: 40.00, unit: WEIGHT_UNIT },
+    ],
+    name: "Karma Lemon CBD",
+    grammarges: [
+        { grammarge: 1.5, price: 4.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 8.00, unit: WEIGHT_UNIT },
+        { grammarge: 10, price: 22.00, unit: WEIGHT_UNIT },
+        { grammarge: 25, price: 40.00, unit: WEIGHT_UNIT },
+    ],
+    name: "Purple",
+    grammarges: [
+        { grammarge: 1.5, price: 4.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 8.00, unit: WEIGHT_UNIT },
+        { grammarge: 10, price: 22.00, unit: WEIGHT_UNIT },
+        { grammarge: 25, price: 40.00, unit: WEIGHT_UNIT },
+    ],
+    name: "Cbg dream",
+    grammarges: [
+        { grammarge: 1.5, price: 4.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 8.00, unit: WEIGHT_UNIT },
+        { grammarge: 10, price: 22.00, unit: WEIGHT_UNIT },
+        { grammarge: 25, price: 40.00, unit: WEIGHT_UNIT },
+    ],
+    name: "Hemp Resin Hash",
+    grammarges: [
+        { grammarge: 1, price: 4.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 12.00, unit: WEIGHT_UNIT },
+    ],
+    name: "Magic Charas",
+    grammarges: [
+        { grammarge: 1, price: 6.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 15.00, unit: WEIGHT_UNIT },
+    ],
+    name: "Choco Hash CBG",
+    grammarges: [
+        { grammarge: 1, price: 6.00, unit: WEIGHT_UNIT },
+        { grammarge: 3, price: 15.00, unit: WEIGHT_UNIT },
+    ],
+}
+]
+const HEMP_COSMETIC = [
+    {
+        name: "Cleansing fluid with CBD",
+        price: 10.50,
+    },
+    {
+        name: "Lip Balm with CBD",
+        price: 6.50,
+    },
+    {
+        name: "CBD Shea Butter with calendula and ajenuz",
+        price: 14.50,
+    },
+    {
+        name: "Regenerating Moisturizing Cream with CBD",
+        price: 14.50,
+    },
+    {
+        name: "Eye contour serum with CBD",
+        price: 14.50,
+    },
+    {
+        name: "Anti-wrinkle serum with CBD",
+        price: 14.50,
+    },
+    {
+        name: "CBD Tattoo Balm",
+        price: 14.50,
+    },
+    {
+        name: "Calming Balm with CBD",
+        price: 14.50,
+    },
+    {
+        name: "Body Milk with CBD",
+        price: 10.50,
+    },
+]
 const ACEITE_CBD = [
     {
         name: "Aceite 5% CBD",
