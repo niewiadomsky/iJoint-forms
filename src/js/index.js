0214e8e2fcad71464d800a0d01af23d73fc64caa
DawidRokita <dawidrokita11@gmail.com>
2021-07-29 10:58:23 +0200

    Aceite CBD, alimentacion

---
+++ src/js/index.js
@@ -0,0 +1,113 @@
+const WEIGHT_UNIT = "gr"
+const LIQUID_UNIT = "ml"
+
+const ACEITE_CBD = [
+    {
+        name: "Aceite 5% CBD",
+        grammarges:[
+            {grammarge: 10, price: 10.00, unit: LIQUID_UNIT},
+            {grammarge: 30, price: 30.00, unit: LIQUID_UNIT}
+        ]
+    },
+    {
+        name: "Aceite 10% CBD",
+        grammarges:[ 
+            {grammarge: 10, price: 20.00, unit: LIQUID_UNIT},
+            {grammarge: 30, price: 60.00, unit: LIQUID_UNIT}
+        ]
+    },
+    {
+        name: "Aceite 15% CBD",
+        grammarges:[
+            {grammarge: 10, price: 30.00, unit: LIQUID_UNIT},
+            {grammarge: 30, price: 80.00, unit: LIQUID_UNIT}
+        ]
+    },
+    {    
+        name: "Aceite 20% CBD",
+        grammarges:[
+            {grammarge: 10, price: 38.00, unit: LIQUID_UNIT},
+            {grammarge: 30, price: 90.00, unit: LIQUID_UNIT}
+        ]
+    },
+    {    
+        name: "Aceite 30% CBD",
+        grammarges:[
+            {grammarge: 10, price: 68.00, unit: LIQUID_UNIT},
+            {grammarge: 30, price: 170.00, unit: LIQUID_UNIT}
+        ]
+    },
+    {
+        name: "Aceite CBD DOG",
+        grammarges:[
+            {price: 10.00}
+        ]
+    }
+]
+
+const ALIMENTATION = [
+    {
+        name: "Caramelos de CBD",
+        grammarges:[
+            {grammarge: 50, price: 3.75, unit: WEIGHT_UNIT},
+            {grammarge: 100, price: 7.50, unit: WEIGHT_UNIT}
+        ]
+    },
+    {
+        name: "Gominolas de cáñamo Fresa",
+        grammarges:[
+            {grammarge: 50, price: 3.75, unit: WEIGHT_UNIT},
+            {grammarge: 100, price: 7.50, unit: WEIGHT_UNIT}
+        ]
+    },
+    {
+        name: "Gominolas de cáñamo Manzana Verde",
+        grammarges:[
+            {grammarge: 50, price: 3.75, unit: WEIGHT_UNIT},
+            {grammarge: 100, price: 7.50, unit: WEIGHT_UNIT}
+        ]
+    },
+    {
+        name: "Gominolas de cáñamo Regaliz",
+        grammarges:[
+            {grammarge: 50, price: 3.75, unit: WEIGHT_UNIT},
+            {grammarge: 100, price: 7.50, unit: WEIGHT_UNIT}
+        ]
+    },
+    {
+        name: "Chocolate",
+        grammarges:[
+            {price: 5.50}
+        ]
+    },
+    {
+        name: "Grinded Hemp",
+        grammarges:[
+            {grammarge: 40, price: 5.50, unit: WEIGHT_UNIT}
+        ]
+    },
+    {
+        name: "Proteina Hemp",
+        grammarges:[
+            {price: 5.50}
+        ]
+    },
+    {
+        name: "Hemp Infusión Piramide",
+        grammarges:[
+            {price: 5.50}
+        ]
+    },
+    {
+        name: "Cañamon Pelado",
+        grammarges:[
+            {price: 5.50}
+        ]
+    },
+    {
+        name: "Picadura",
+        grammarges:[
+            {grammarge: 25, price: 5.50, unit: WEIGHT_UNIT}
+        ]
+    }
+]
